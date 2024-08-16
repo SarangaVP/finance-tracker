21cd62332716e47d3b16b7d4b1c2764c1a8d62a4
Saranga Malshan <sarangavp36@gmail.com>
2024-08-16 23:14:55 +0530

    Setup mongodb connection

---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+
+const mongoURI: string | undefined = process.env.MONGO_URI;
+
+if (!mongoURI) {
+  throw new Error('MONGO_URI is not defined');
+}
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("CONNECTED TO MONGODB!"))
+  .catch((err) => console.error("Failed to Connect to MongoDB:", err));
+
+app.listen(port, () => {
+    console.log(`Server Running on Port ${port}`);
+});

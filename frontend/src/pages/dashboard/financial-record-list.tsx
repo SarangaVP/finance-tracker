ec030efcfcb579f47abe13d1dfe041f2d3738381
Saranga Malshan <sarangavp36@gmail.com>
2024-08-16 18:00:36 +0530

    Create record form

---
+++ frontend/src/pages/dashboard/financial-record-list.tsx
@@ -0,0 +1,5 @@
+export const FinancialRecordList = () => {
+    return <div>
+        <h1>Record List</h1>
+    </div>
+}
--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,3 +1,14 @@
+import { useUser } from "@clerk/clerk-react"
+import { FinancialRecordForm } from "./financial-record-form";
+import { FinancialRecordList } from "./financial-record-list";
+
+
+
 export const Dashboard = () => {
-    return <h1>This is the dashboard</h1>
+    const {user} = useUser();
+    return <div className="dashboard-container">
+        <h1> Welcome {user?.firstName}! Here are your Finances:</h1>
+        <FinancialRecordForm/>
+        <FinancialRecordList/>
+    </div>
 }

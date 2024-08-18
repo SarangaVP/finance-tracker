41207c4bbf5863374a419d07a7dd324c8121d437
Saranga Malshan <sarangavp36@gmail.com>
2024-08-18 14:13:29 +0530

    Update record-context

--- frontend/src/contexts/financial-record-context.tsx
+++ frontend/src/contexts/financial-record-context.tsx
@@ -20,6 +20,23 @@ export const FinancialRecordsProvider = ({children,}: {children: React.ReactNode
     const [records, setRecords] = useState<FinancialRecord[]>([]);
     const { user } = useUser();
 
+    const fetchRecords = async () => {
+        if (!user) return;
+        const response = await fetch(
+          `http://localhost:8010/financial-records/getAllByUserID/${user.id}`
+        );
+    
+        if (response.ok) {
+          const records = await response.json();
+          console.log(records);
+          setRecords(records);
+        }
+      };
+    
+      useEffect(() => {
+        fetchRecords();
+      }, [user]);
+
     const addRecord = async (record: FinancialRecord) => {
         const response = await fetch("http://localhost:8010/financial-records", {
           method: "POST",
@@ -34,7 +51,9 @@ export const FinancialRecordsProvider = ({children,}: {children: React.ReactNode
             const newRecord = await response.json();
             setRecords((prev) => [...prev, newRecord]);
           }
-        } catch (err) {}
+        } catch (err) {
+            console.log(err);
+        }
       };
      
     return (
